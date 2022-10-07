d6e78e10e03ce2fed9a8de953c303df5409376ff
Chi Chiu Lam <lamcc90@gmail.com>
2022-10-07 05:14:45 -0400

    more animations added

--- public/js/animation.js
+++ public/js/animation.js
@@ -4,3 +4,22 @@ gsap.from(".bg-img", {
   ease: "sine.out",
   y: -500,
 });
+
+// for hero text
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".hero"
+  }
+});
+// tl.from(".text-container", {y: 250, opacity: 0, duration: 3})
+tl.from(".hp-welcome", {y: 250, opacity: 0, duration: 1.8})
+tl.from(".hp-welcome-intro", {y: 150, opacity: 0, duration: 1.4})
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const smoother = ScrollSmoother.create({
+ content: ".card",
+ smooth: 3,
+ effects: true
+});
