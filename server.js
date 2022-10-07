b2fbecc54d2c2d46f9e4ce05fb531c807fae6f01
Chi Chiu Lam <lamcc90@gmail.com>
2022-10-06 23:44:57 -0400

    .env changes

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-// const helpers = require("./utils/helpers");
 const hbsHelpers = require("./utils/hbsHelpers");
 
 const sequelize = require("./config/connection");
