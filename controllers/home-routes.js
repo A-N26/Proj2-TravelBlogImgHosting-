d3be9d94356211b34c02259c8f650888c03396d3
A-N26 <108244197+A-N26@users.noreply.github.com>
2022-10-11 04:36:12 -0400

    file additions and certain edits.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 
+router.get('/', function (req, res) {
+    res.render('testing');
+});
+
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -6,13 +6,13 @@ const seedComments = require('./comment-seeds');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
-    console.log(chalk.blue.bold(figlet.textSync(`\n----- DATABASE SYNCED -----\n`)));
+    console.log(`\n----- DATABASE SYNCED -----\n`);
     await seedUsers();
-    console.log(chalk.green.italic(`\n----- USERS SYNCED -----\n`));
+    console.log(`\n----- USERS SYNCED -----\n`);
     await seedPosts();
-    console.log(chalk.yellow.italic(`\n----- POSTS SYNCED -----\n`));
+    console.log(`\n----- POSTS SYNCED -----\n`);
     await seedComments();
-    console.log(chalk.red.italic(`\n----- COMMENTS SYNCED -----\n`));
+    console.log(`\n----- COMMENTS SYNCED -----\n`);
 
     process.exit(0);
 };
